1650581f128fd8ff8b212dc59442c888abdee292
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-02-12 19:45:11 +0530

    loop

---
+++ 4 loops in js/script.js
@@ -0,0 +1,19 @@
+//loops in js
+//for loop
+for (let i=0; i<5; i++){
+    console.log("welcome from Rohit..");
+}
+
+sum=0;
+n=5;
+for(let i=1; i<=n; i++){
+    sum=sum+i;
+}
+console.log(sum);
+
+//while loop
+let j=1;
+while(j <=10){
+    console.log(j);
+    j++;
+}
