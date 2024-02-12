d92d07a9cac70c6673fc8cd97b1126444d6b6620
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-02-12 10:19:55 +0530

    operators

---  2 operators and conditional statements/script.js
+++  2 operators and conditional statements/script.js
@@ -1,3 +1,55 @@
 console.log('happy birthday');
 age=22;
 console.log(age);
+/*operators
+Arithmatic opeators +,-,*,/
+*/
+let a=10;
+let b=20;
+console.log(" a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a % b = ", a%b);  //modulus
+console.log("a ** b = ", b**a);  //exponential
+//unary operators
+
+a=a+1;
+console.log('a=',a)
+console.log('a++=',++a); //pre-increment
+console.log('++a=',a++);  //post-increment
+console.log('a',a);
+
+//Assignment operators(=,+=,-=,%=,**=)
+a += 4;
+console.log('a=',a);
+
+a -= 4;
+console.log('a=',a);
+
+a *= 4;
+console.log('a=',a);
+
+a /= 4;
+console.log('a=',a);
+
+a **= 4;
+console.log('a=',a);
+
+//comparison operators(==,===,!=,!==,>,>=,<,<=)
+
+c=5;
+d='5';
+console.log("4==5",4==5);
+console.log('c === d ', c===d); //value and dtype
+console.log('3 !=3 ',3 !=3);
+console.log('5<6 ', 5<6);
+console.log('5>6 ', 5>6);
+console.log('5<=6 ', 5<=6);
+console.log('5>=6 ', 5>=6);
+
+//logical operators(&&,||,!)
+console.log("5<6 && 4>3",5<6 && 4>3);
+console.log("5<6 || 4>6",5<6 || 4>6);
+console.log("!(5<6) ",!(5<6));
+
