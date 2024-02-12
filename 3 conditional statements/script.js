d9157183bd7b2da559686ab688692a0e670080bb
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-02-12 10:30:41 +0530

    conditional statements

---
+++ 3 conditional statements/script.js
@@ -0,0 +1,16 @@
+//conditional statements
+let age = 22;
+if(age > 18) {
+    console.log("you can vote");
+}
+
+let mode = "dark";
+let color;
+
+if(mode === 'dark'){
+    color="black"
+}
+if(mode === "light"){
+    color="white"
+}
+console.log(color);
