9200ee7b7fdc8404be5df41660a06b30ba6bf37d
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-02-15 22:32:42 +0530

    selector in js

--- 8 dom/script.js
+++ 8 dom/script.js
@@ -1,1 +1,23 @@
 console.log("hello js learner...");
+
+//element selectors in js
+//by id
+let heading=document.getElementById("heading1")
+console.dir(heading);
+console.log(heading);
+
+//by class
+let heading2=document.getElementsByClassName("heading2")
+console.dir(heading2);
+console.log(heading2);
+
+//by tagname
+let para=document.getElementsByTagName("p")
+console.dir(para);
+console.log(para);
+
+//by queryselector(id,class,elemet)
+let p=document.querySelector("p");
+console.dir(p);
+let h1=document.querySelector("#heading1");
+console.dir(h1);
