94d0405513962c4cd1f55d978be9f5d6863ab712
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-02-15 23:44:12 +0530

    dom properties

--- 8 dom/script.js
+++ 8 dom/script.js
@@ -21,3 +21,41 @@ let p=document.querySelector("p");
 console.dir(p);
 let h1=document.querySelector("#heading1");
 console.dir(h1);
+
+//DOM manupulation Properties
+
+//tagName  returns tag for element nodes
+let tagname=document.querySelector("div").tagName;
+console.dir(tagname);
+console.log(tagname);
+
+//innerText returns text content of the elements and all its children
+let intext=document.querySelector("div").innerText;
+console.dir(intext);
+console.log(intext);
+
+//innerHTML returns plain text and html content in the element
+let inrtext=document.querySelector("div").innerHTML;
+console.dir(inrtext);
+console.log(inrtext);
+
+//textContent returns textual content even for hidden elements
+let content = document.querySelector("div").textContent;
+console.log(content);
+
+let h2=document.querySelector("h2");
+h2.innerText=h2.innerText+" learning with spirit";
+console.dir(h2);
+
+let div=document.querySelectorAll(".div");
+div[0].innerText="first div";
+div[1].innerText="second div";
+div[2].innerText="third div";
+console.dir(div);
+
+
+let idx=1;
+for (d of div){
+    d.innerText = `div-${idx}`;
+    idx++;
+}
