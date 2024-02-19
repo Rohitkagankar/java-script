f12f24cf2f6225290f7590eeb5ce75d872f5e0f9
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-02-19 19:05:57 +0530

    promises

--- 13 promises/script.js
+++ 13 promises/script.js
@@ -9,6 +9,70 @@ function getData(dataId,getNextData){
             }
         },5000);
     });
+};
+let promises=new getData(123);
+console.log(promise);
+
+//------------
+
+const getPromise = ()=>{
+    return new Promise((resolve,reject)=>{
+        console.log("promise is delivered...");
+        resolve("success");
+        // reject("error");
+    });
+};
+
+let promise = getPromise();
+
+
+promise.then((res)=>{
+    console.log("promise fulfilled",res);
+});
+
+promise.catch((err)=>{
+    console.log("rejected",err);
+});
+
+
+//--------------
+
+function asyncFunc1(){
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            console.log("data1");
+            resolve("success");
+        },4000);
+    });
 }
-// let promise=new getData(123);
-// console.log(promise);
+
+function asyncFunc2(){
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            console.log("data2");
+            resolve("success");
+        },4000);
+    });
+}
+
+
+console.log("fetching data1...");
+let p1 = asyncFunc1();
+p1.then((res)=>{
+    
+    console.log("fetching data2...");
+    let p2 =asyncFunc2();
+    p2.then((res)=>{
+        
+    });
+});
+
+asyncFunc1().then((res)=>{
+    return asyncFunc2();
+});
+
+console.log("fetching data2...");
+let p2 = asyncFunc2();
+p2.then((res)=>{
+    console.log(res);
+});
