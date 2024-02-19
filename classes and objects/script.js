2b8d12f30174027a38bf941da5762dac19ee8c3c
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-02-19 09:08:03 +0530

    inheritance

--- classes and objects/script.js
+++ classes and objects/script.js
@@ -27,3 +27,35 @@ class tataCar{
 //object of class
 let fortuner =new tataCar();
 fortuner.setBrand("fortuner");
+
+//inheritance
+
+
+class parent{
+    hello(){
+        console.log("hello");
+    }
+}
+class child extends parent{
+
+}
+let obj1= new child();
+
+class company{
+    comName(){
+        console.log("Google");
+    }
+}
+class empl extends company{
+    name(name){
+        console.log(`name of emp is ${name}`);
+    }
+    salary(){
+        console.log("20 lpa");
+    }
+}
+let emp1 = new empl();
+emp1.name("Rohit");
+console.log(emp1.name("Ranjit"));
+console.log(emp1.comName());
+
